4cd905b64a886cf02f467fe38aa956d53703bb1c
Dmitry Shapran <djmitrofanov@gmail.com>
2024-07-13 13:18:49 +0300

    port 0000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || '0.0.0.0';
+const port = process.env.PORT || 0000;
 
 server.use(middlewares);
 server.use(router);
